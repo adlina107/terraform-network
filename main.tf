50b43ee48ccb0855218a9266549c5e40e6d60fff
adlina107 <120630035+adlina107@users.noreply.github.com>
2023-09-26 14:30:34 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,23 @@ variable "instance_name" {}
 variable "instance_zone" {}
 variable "instance_type" {
   default = "e2-standard-2"
-}
+  }
 variable "instance_subnetwork" {}
+
 resource "google_compute_instance" "vm_instance" {
-name = var.instance_name
-#RESOURCE properties go here
-zone         = var.instance_zone
-machine_type = var.instance_type
+  name         = "${var.instance_name}"
+  zone         = "${var.instance_zone}"
+  machine_type = "${var.instance_type}"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-    }
+      }
   }
   network_interface {
-    subnetwork = var.instance_subnetwork
+    subnetwork = "${var.instance_subnetwork}"
     access_config {
       # Allocate a one-to-one NAT IP to the instance
     }
-  }  
+  }
+  allow_stopping_for_update = true
 }
