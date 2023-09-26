30568eb3becf9caaf26f5cf985ee8a692163f018
adlina107 <120630035+adlina107@users.noreply.github.com>
2023-09-26 14:33:14 +0800

    Create mynetwork.tf

---
+++ mynetwork.tf
@@ -0,0 +1,35 @@
+# Create the mynetwork network
+resource "google_compute_network" "mynetwork" {
+  name = "mynetwork"
+  #RESOURCE properties go here
+  auto_create_subnetworks = "true"
+}
+# Create a firewall rule to allow HTTP, SSH, RDP and ICMP traffic on mynetwork
+resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
+  name = "mynetwork-allow-http-ssh-rdp-icmp"
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+  network = google_compute_network.mynetwork.self_link
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "3389"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+}
+# Create the mynet-us-vm instance
+module "mynet-us-vm" {
+  source              = "./instance"
+  instance_name       = "mynet-us-vm"
+  instance_zone       = "us-west1-c"
+  instance_subnetwork = google_compute_network.mynetwork.self_link
+}
+# Create the mynet-second-vm" instance
+module "mynet-second-vm" {
+  source              = "./instance"
+  instance_name       = "mynet-second-vm"
+  instance_zone       = "us-east4-b"
+  instance_subnetwork = google_compute_network.mynetwork.self_link
+}
